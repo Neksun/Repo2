2a2935565be287629f92f5697dbfb428fdf8eb05
Neksun <152362111+Neksun@users.noreply.github.com>
2024-08-12 22:26:30 -0500

    Add files via upload

---
+++ challenge2.js
@@ -0,0 +1,13 @@
+class TODO{
+    constructor(title, description, next=null){
+        this.title= title;
+        this.description= description;
+        this.next=next;
+
+    }
+}
+const TODO1 = new TODO ("the cat", "this is a cat",)
+const TODO2 = new TODO ("the dog", "this is a dog", )
+const TODO3 = new TODO ("the bird", "this is a bird", )
+
+
