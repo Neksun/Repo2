43936d72395f6cb14e4bdec0d308d66542c2b94e
Neksun <152362111+Neksun@users.noreply.github.com>
2024-08-14 15:48:38 -0500

    Add files via upload

---
+++ StackCallenge2.js
@@ -0,0 +1,16 @@
+class Books{
+    constructor(Name,ISBN,Author,Editorial){
+        this.Name=Name
+        this.ISBN=ISBN
+        this.Author=Author
+        this.Editorial=Editorial
+
+    }
+}
+
+let Book1= new Books('Crepusculo',1,'Charlote','el buho');
+let Book2= new Books('Cencicienta',2,'WaltDisney','Disney');
+let Book3= new Books('geissha del futuro',3,'Victoria','el buho');
+let Book4= new Books('empresario atrevido',4,'jhon lenon','casa bonita');
+let Book5= new Books('humana a conejo',5,'yukio','casa bonita');
+let Book6= new Books('humana a pajaro',6,'yukio','casa bonita');
