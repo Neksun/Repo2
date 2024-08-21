283c41910a45d838883fdba1552de551d6cb5bc2
Neksun <152362111+Neksun@users.noreply.github.com>
2024-08-20 21:22:27 -0500

    Update StackCallenge2.js

--- StackCallenge2.js
+++ StackCallenge2.js
@@ -1,3 +1,4 @@
+/////////Karol payares 2221343
 class Books{
     constructor(Name,ISBN,Author,Editorial){
         this.Name=Name
@@ -14,3 +15,16 @@ let Book3= new Books('geissha del futuro',3,'Victoria','el buho');
 let Book4= new Books('empresario atrevido',4,'jhon lenon','casa bonita');
 let Book5= new Books('humana a conejo',5,'yukio','casa bonita');
 let Book6= new Books('humana a pajaro',6,'yukio','casa bonita');
+
+
+function printTODOs(todo) {
+    while (todo) {
+      console.log("Title:", todo.title);
+      console.log("Description:", todo.description);
+      console.log("Points to:", todo.pointsToNext ? todo.pointsToNext.title : "None");
+      console.log("\n");
+      todo = todo.pointsToNext;
+    }
+  }
+
+printTODOs(todo);
