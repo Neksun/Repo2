f7651477b4c6099605adadba65544eb5499bd2b3
Neksun <152362111+Neksun@users.noreply.github.com>
2024-08-21 11:08:56 -0500

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -1,3 +1,4 @@
+//Karol payares 2221343
 class TODO{
     constructor(title, description, next=null){
         this.title= title;
@@ -7,15 +8,17 @@ class TODO{
     }
 }
 const TODO1 = new TODO ("the cat", "this is a cat",)
-const TODO2 = new TODO ("the dog", "this is a dog", )
-const TODO3 = new TODO ("the bird", "this is a bird", )
+const TODO2 = new TODO ("the dog", "this is a dog", TODO1 )
+const TODO3 = new TODO ("the bird", "this is a bird", TODO2 )
 
 function TODOs(head) {
     let current = head;
     while (current !== null) {
       console.log(`Title: ${current.title}`);
       console.log(`Description: ${current.description}`);
-      console.log("----");
+      console.log("////");
       current = current.next;
     }
   }
+
+TODOs(TODO3);
