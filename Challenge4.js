baad08d85ae9062219b8c694751382543677cd0f
Neksun <152362111+Neksun@users.noreply.github.com>
2024-08-28 07:57:43 -0500

    Update Challenge4.js

--- Challenge4.js
+++ Challenge4.js
@@ -3,60 +3,72 @@ class Person {
     this.name = name;
     this.age = age;
     this.city = city;
+    this.edges = [];  // Lista de amigos (otros nodos conectados)
   }
 }
 
 class City {
   constructor(name) {
     this.name = name;
+    this.residents = [];  // Lista de personas que viven en esta ciudad
+  }
+
+  addResident(person) {
+    this.residents.push(person);
   }
 }
 
 class Graph {
   constructor() {
-    this.nodes = {};
+    this.people = {};  // Almacena las personas por nombre
+    this.cities = {};  // Almacena las ciudades por nombre
   }
 
-  addNode(node) {
-    this.nodes[node.name] = node;
-  }
-
-  addEdge(node1, node2) {
-    this.nodes[node1.name].edges.push(node2.name);
-    this.nodes[node2.name].edges.push(node1.name);
-  }
-
-  getPeopleInCity(city) {
-    const people = [];
-    for (const node in this.nodes) {
-      if (this.nodes[node] instanceof Person && this.nodes[node].city === city) {
-        people.push(this.nodes[node]);
-      }
+  addPerson(person) {
+    this.people[person.name] = person;
+    if (this.cities[person.city]) {
+      this.cities[person.city].addResident(person);
     }
-    return people;
+  }
+
+  addCity(city) {
+    this.cities[city.name] = city;
+  }
+
+  addEdge(person1, person2) {
+    this.people[person1.name].edges.push(person2.name);
+    this.people[person2.name].edges.push(person1.name);
+  }
+
+  printResidents(cityName) {
+    if (this.cities[cityName]) {
+      console.log(`People living in ${cityName}:`);
+      this.cities[cityName].residents.forEach(resident => {
+        console.log(`${resident.name}, Age: ${resident.age}`);
+      });
+    } else {
+      console.log(`City ${cityName} not found.`);
+    }
   }
 }
 
+// Ejemplo de uso:
+let graph = new Graph();
 
-const graph = new Graph();
+let city1 = new City("New York");
+let city2 = new City("Los Angeles");
 
+graph.addCity(city1);
+graph.addCity(city2);
 
-const alice = new Person('Alice', 30, 'New York');
-const bob = new Person('Bob', 25, 'Los Angeles');
-const charlie = new Person('Charlie', 35, 'New York');
-const newYork = new City('New York');
-const losAngeles = new City('Los Angeles');
+let person1 = new Person("Alice", 30, "New York");
+let person2 = new Person("Bob", 25, "Los Angeles");
+let person3 = new Person("Charlie", 35, "New York");
 
-graph.addNode(alice);
-graph.addNode(bob);
-graph.addNode(charlie);
-graph.addNode(newYork);
-graph.addNode(losAngeles);
+graph.addPerson(person1);
+graph.addPerson(person2);
+graph.addPerson(person3);
 
+graph.addEdge(person1, person3);
 
-graph.addEdge(alice, newYork);
-graph.addEdge(bob, losAngeles);
-graph.addEdge(charlie, newYork);
-
-const peopleInNewYork = graph.getPeopleInCity('New York');
-console.log(peopleInNewYork);
+graph.printResidents("New York");
