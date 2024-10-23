704528333a0d4660d95ad56be65c96b36be3e914
Neksun <152362111+Neksun@users.noreply.github.com>
2024-10-23 14:08:06 -0500

    Add files via upload

---
+++ TodoApp.js
@@ -0,0 +1,35 @@
+import React, { useReducer } from 'react';
+import { TodoReducer } from './components/TodoReducer';
+import Header from './components/Encabezado';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
+const initialState = [
+  {
+    id: new Date().getTime(),
+    description: 'Hacer los challenges',
+    done: false,
+  },
+];
+
+export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(TodoReducer, initialState);
+
+  const handleAddTodo = (newTodo) => {
+    dispatch({ type: 'add', payload: newTodo });
+  };
+
+  const handleDelete = (id) => {
+    dispatch({ type: 'delete', payload: id });
+  };
+
+  return (
+    <>
+      <Header todos={todos} />
+      <div className="row">
+        <TodoList todos={todos} handleDelete={handleDelete} />
+        <TodoForm handleAddTodo={handleAddTodo} />
+      </div>
+    </>
+  );
+};
